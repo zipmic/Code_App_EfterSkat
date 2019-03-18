7e6a77d63475c54a7e9635398c448da43e5a9739
Christian Kragegaard <mail@code.dk>
2019-03-18 22:14:18 +0100

    finAL

--- Assets/AppController.cs
+++ Assets/AppController.cs
@@ -13,13 +13,56 @@ public class AppController : MonoBehaviour {
 
 	private float procent = 38f;
 
+    private float currentMoneyInput;
+
+    private float lon, ambidrag, skat, finalmoney;
+
+
 	// Use this for initialization
 	void Start ()
 	{
 		
 
 	}
-	
+
+    public void InputValueChange()
+    {
+        currentMoneyInput = float.Parse(MoneyField.text);
+
+        lon = currentMoneyInput;
+
+        ambidrag = 0.08f * lon;
+
+        skat = (procent/100) * (lon - ambidrag);
+
+        finalmoney = lon - ambidrag - skat;
+
+        //LeftText = "Løn:\nAM-bidrag:\nA-skat:";
+        RightText.text = lon.ToString() + " DKK" + "\n" + ambidrag.ToString() + " DKK" + "\n" + skat.ToString() + " DKK" + "\n";
+        UdbetaltText.text = "Udbetalt efter skat:\n"+finalmoney + " DKK";
+
+    }
+
+
+
+    public void PlusProcent()
+    {
+        if(procent < 100)
+        {
+            procent++;
+            ProcentText.text = procent + "%";
+        }
+    }
+
+    public void MinusProcent()
+    {
+        if (procent > 0)
+        {
+            procent--;
+            ProcentText.text = procent + "%";
+        }
+    }
+
 
 
 
