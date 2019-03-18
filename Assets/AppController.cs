73924ae497a4ebbae3c063982c711c72b24f1004
Christian Skriver Kragegaard <mail@code.dk>
2019-03-18 20:48:04 +0100

    init setup

---
+++ Assets/AppController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AppController : MonoBehaviour {
+
+
+	public Text ProcentText, LeftText, RightText, UdbetaltText;
+	public InputField MoneyField;
+
+
+	private float procent = 38f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+
+	}
+	
+
+
+
+}
